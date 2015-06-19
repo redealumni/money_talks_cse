cb80188cad236d2aafa201e61bdcfab3c9378676
Felipe JAPM <felipe.japm@gmail.com>
2015-06-19 12:40:45 -0300

    Adds reader accessor to errors

--- lib/money_talks/errors.rb
+++ lib/money_talks/errors.rb
@@ -3,6 +3,8 @@ module MoneyTalks
   class PaymentNotImplementedError < NameError; end
   class AdyenError < StandardError
 
+      attr_reader :code, :original_message
+
       def initialize(code, original_message)
         @code, @original_message = code, original_message
         super(MoneyTalks.translation[:errors][code])
